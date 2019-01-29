7eed049e777c2cda9d14681732d1b0621c1b32e4
Eddie Franco <eddiefranco626@gmail.com>
2019-01-28 19:12:14 -0600

    Updates

---
+++ precache-manifest.e6ba5624e74623e873674089eb6b5591.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f9f28a563713d1709156346cefbd703",
+    "url": "/hp-memory-game/static/media/hagrid.1f9f28a5.jpg"
+  },
+  {
+    "revision": "86a71e89acffb770cc00",
+    "url": "/hp-memory-game/static/css/main.cbed0fc4.chunk.css"
+  },
+  {
+    "revision": "6aa4c3fdb43c4b1063bd",
+    "url": "/hp-memory-game/static/js/1.6aa4c3fd.chunk.js"
+  },
+  {
+    "revision": "61c9e3d9be92ee002215",
+    "url": "/hp-memory-game/static/js/runtime~main.61c9e3d9.js"
+  },
+  {
+    "revision": "52786311be165fb99150316cef748bc3",
+    "url": "/hp-memory-game/static/media/hpBackground3.52786311.jpg"
+  },
+  {
+    "revision": "df988a88b1db7c1b9fcb92f6d2b6b99c",
+    "url": "/hp-memory-game/static/media/harryPotter.df988a88.jpg"
+  },
+  {
+    "revision": "32e86c53d4efb4e69934c6347a9d4a00",
+    "url": "/hp-memory-game/static/media/dobby.32e86c53.jpg"
+  },
+  {
+    "revision": "0fea9b0404582bcc4c630099fbc2506f",
+    "url": "/hp-memory-game/static/media/ginnyWeasley.0fea9b04.jpg"
+  },
+  {
+    "revision": "86a71e89acffb770cc00",
+    "url": "/hp-memory-game/static/js/main.86a71e89.chunk.js"
+  },
+  {
+    "revision": "edfe27667afeae114c4166e92fcd49c6",
+    "url": "/hp-memory-game/static/media/lunaLovegood.edfe2766.jpg"
+  },
+  {
+    "revision": "8fe52ac44952419f9a2fa6ca644e9977",
+    "url": "/hp-memory-game/static/media/nevilleLongbottom.8fe52ac4.jpg"
+  },
+  {
+    "revision": "c7c276f7b626309332164ce760760fa5",
+    "url": "/hp-memory-game/static/media/professorMcgonagall.c7c276f7.jpg"
+  },
+  {
+    "revision": "0bfeb75c43e03d1f77e9eccccf42c6e3",
+    "url": "/hp-memory-game/static/media/ronWeasley.0bfeb75c.jpg"
+  },
+  {
+    "revision": "af2edc8b36f2597d563ca3cb73ff1eb1",
+    "url": "/hp-memory-game/static/media/severusSnape.af2edc8b.jpg"
+  },
+  {
+    "revision": "38bbf5c6e8b547de5ed8af9c1c705ce9",
+    "url": "/hp-memory-game/static/media/dracoMalfoy.38bbf5c6.jpg"
+  },
+  {
+    "revision": "ddadd404d0b49f6516cdae2eadb989dc",
+    "url": "/hp-memory-game/static/media/voldemort.ddadd404.jpg"
+  },
+  {
+    "revision": "6c8cb305f7c9160ef4dc9f870f97525d",
+    "url": "/hp-memory-game/index.html"
+  }
+];
